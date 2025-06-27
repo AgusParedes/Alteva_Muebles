9679ba1ea2333d5a541653da09af0713415faae7
AGUSTIN PAREDES <paredesagustin15@gmail.com>
2025-06-27 11:07:32 -0300

    Arreglo firebase

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -2,15 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAgNvYqUNCVJRZSzBZp7JMpaJ7BxkM70Bc",
-  authDomain: "alteva-92d8b.firebaseapp.com",
-  projectId: "alteva-92d8b",
-  storageBucket: "alteva-92d8b.appspot.com",
-  messagingSenderId: "497258250057",
-  appId: "1:497258250057:web:a6583f2090d6b0667e2498",
-  measurementId: "G-GB6YWLSFF5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_measurementId
 };
 
 const app = initializeApp(firebaseConfig);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, 
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
   },
 })
